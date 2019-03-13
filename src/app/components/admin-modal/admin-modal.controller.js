4e09a64aca1aee62293652be6f58a380b000ba2a
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-03-13 13:09:46 +0200

    feature/home *update formatting dates *update rendering dates in app

--- src/app/components/admin-modal/admin-modal.controller.js
+++ src/app/components/admin-modal/admin-modal.controller.js
@@ -1,14 +1,17 @@
 'use strict';
 
-function AdminModalController(sharedAdminFactory, $scope, syncDataService) {
+function AdminModalController(sharedAdminFactory, $scope, syncDataService, workWithDate) {
     'ngInject';
 
     const self = this;
     
     $scope.$watch(() => sharedAdminFactory.userData, () => {
         // const userID = sharedAdminFactory.getUserData();
-        const userID = '3vesXZTo6tYLgevTBY3aLdZ26TB2';
+        const userID = '7sikAxSigSZGC6v6FezOSeaNEPw2';
         self.data = syncDataService.getCheckedUserDealsFromFirebase(userID);
+        self.data.$loaded(() => {
+            workWithDate.transformData(self.data)
+          });
     });
 }
 
