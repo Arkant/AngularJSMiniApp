e5afc8b3d77a56d52bf2f6a2b2388839519573fe
Denis Derkach <podstavka128@gmail.com>
2019-03-13 22:58:50 +0200

    feature/table/denis, create draft

--- src/app/core/directives/table/table.directive.js
+++ src/app/core/directives/table/table.directive.js
@@ -11,23 +11,33 @@ export default function (app) {
       restrict: 'E',
       template: `
             <table class="table table-striped rounded table-hover">
-            <thead>
-              <tr>
-                 <th  custom-sort order="'date'" sort="sort" ng-repeat="item in headers" >{{item}}
-               </th>             
-              </tr>
-            </thead>
-      
-            <tbody>
-              <tr class="table-row" ng-repeat="el in data | orderBy:sort.sortingOrder:sort.reverse">
-                <td ng-repeat='item in el '>{{ item}}</td>
-            </tbody>
-          </table>
+              <thead>
+                <tr>
+                  <th ng-repeat="element in data.config" >{{ element.header }}</th>             
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="table-row" ng-repeat="el in data.data">
+
+                  <td ng-repeat="(key, value) in el" ng-if="data.config[key].type === 'text'">
+                    {{ value }}
+                  </td>
+
+                  <td ng-repeat="(key, value) in el" ng-if="data.config[key].type === 'checkbox'">
+                    <input ng-click="data.config[key].callback(value)" type="checkbox">
+                    {{ value }}
+                  </td>
+
+                  <td ng-repeat="(key, value) in el" ng-if="data.config[key].type === 'button'">
+                    <button ng-click="data.config[key].callback(value)">{{ value }}</button>
+                  </td>
+
+                </tr>
+              </tbody>
+            </table>
             `,
       scope: {
-        headers: '=',
-        data: '=',
-        sort:'='
+        data: '='
       },
     };
   }
--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -1,42 +1,53 @@
 'use strict';
 
-function MainController($scope, $filter, $rootScope, syncDataService, $state) {
+function MainController($scope, $rootScope, syncDataService, $state) {
   'ngInject';
+
+  $scope.test = data => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  }
 
   if (!$rootScope.currentUserId) {
     $state.go('sign-up');
   } else {
-    $scope.currentUserDeals = syncDataService.getDealsFromFirebase();
-  }
-
-  $scope.sort = {
-    sortingOrder: 'date',
-    reverse: false
-  };
-  $scope.headers = ['Amount From', 'Amount To', 'Commission', 'Currency From', 'Currency To', 'Date', 'Rate'];
-
-  $scope.filteredItems = [];
-
-  let searchMatch = function (haystack, needle) {
-    if (!needle) {
-      return true;
-    }
-    return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
-  };
-
-  $scope.search = function () {
-    $scope.filteredItems = $filter('filter')($rootScope.currentUserDeals, function (item) {
-      for (let attr in item) {
-        if (searchMatch(item[attr], $scope.query))
-          return true;
+    $scope.currentUserDeals = {
+      data: syncDataService.getDealsFromFirebase(),
+      config: {
+        amountFrom: {
+          header: 'Amount From',
+          type: 'text'
+        },
+        amountTo: {
+          header: 'Amount To',
+          type: 'text'
+        },
+        commission: {
+          header: 'Commission',
+          type: 'text'
+        },
+        currencyFrom: {
+          header: 'Currency From',
+          type: 'text'
+        },
+        currencyTo: {
+          header: 'Currency To',
+          type: 'checkbox',
+          callback: $scope.test
+        },
+        date: {
+          header: 'Date',
+          type: 'button',
+          callback: $scope.test
+        },
+        rate: {
+          header: 'Rate',
+          type: 'button',
+          callback: $scope.test
+        }
       }
-      return false;
-    });
-
-    if ($scope.sort.sortingOrder !== '') {
-      $scope.filteredItems = $filter('orderBy')($scope.filteredItems, $scope.sort.sortingOrder, $scope.sort.reverse);
     }
-  };
+  }
 }
 
 export default MainController;
