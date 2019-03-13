8facdc2d2be94e49d1f3054b4558cbc26aa5fc82
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-03-13 17:03:26 +0200

    feature/home *remove redirection in home page

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -1,10 +1,9 @@
 'use strict';
 
 export default class HomeController {
-  constructor($scope, workWithDate, $rootScope, syncDataService, $state) {
+  constructor(workWithDate, syncDataService, $state) {
     'ngInject';
-    this.scope = $scope;
-    this.rootScope = $rootScope;
+
     this.state = $state;
     this.syncDataService = syncDataService;
     this.workWithDate = workWithDate;
@@ -17,16 +16,10 @@ export default class HomeController {
   }
 
   init() {
-    if (!this.rootScope.currentUserId) {
-      this.state.go('sign-up');
-    } else {
-      this.currentUserDeals = this.syncDataService.getDealsFromFirebase();
-      this.dealsAreLoaded = false;
+    this.currentUserDeals = this.syncDataService.getDealsFromFirebase();
 
-      this.currentUserDeals.$loaded(() => {
-        this.workWithDate.transformData(this.currentUserDeals);
-        this.dealsAreLoaded = true;
-      });
-    }
+    this.currentUserDeals.$loaded(() => {
+      this.workWithDate.transformData(this.currentUserDeals);
+    });
   }
 }
