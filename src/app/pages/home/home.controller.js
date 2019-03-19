48ad3b78b55bae35309fe03db974ed5486c4a7c9
Vladislav <vladislav.shvorobuk@gmail.com>
2019-03-19 13:30:29 +0200

    fix/home, delete scope frome home.controller.js

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -1,12 +1,12 @@
 'use strict';
 
 export default class HomeController {
-  constructor(workWithDate, syncDataService, $scope) {
+  constructor(workWithDate, syncDataService) {
     'ngInject';
 
     this.syncDataService = syncDataService;
     this.workWithDate = workWithDate;
-    $scope.headers = ['Amount From', 'Amount To', 'Commission', 'Currency From', 'Currency To', 'Date', 'Rate'];
+    this.headers = ['Amount From', 'Amount To', 'Commission', 'Currency From', 'Currency To', 'Date', 'Rate'];
 
     this.sort = {
       sortingOrder: 'date',
